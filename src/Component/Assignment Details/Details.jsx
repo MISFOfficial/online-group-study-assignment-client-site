5cf81d9e09268408fd8dc756df3f1d85a5d7437f
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-08-10 07:29:49 +0600

    modify theme

--- src/Component/Assignment Details/Details.jsx
+++ src/Component/Assignment Details/Details.jsx
@@ -16,11 +16,11 @@ const Details = () => {
                 Group study || Details
             </title>
             <div>
-                <div className='bg-purple-800 h-50 md:h-80'>
+                <div className='dark-theme h-50 md:h-80'>
                     <img className=' w-full h-full flex justify-center items-center object-cover' src={assignmentDataId?.thumbnail} alt="" />
                 </div>
                 <div className="flex flex-col justify-center items-center py-10">
-                    <div className={`${theme ? 'bg-purple-800' : 'bg-[#f0f0f0]'} relative flex flex-col items-center rounded-[20px] w-[700px] max-w-[95%] mx-auto  bg-clip-border shadow-3xl shadow-shadow-500  p-3`}>
+                    <div className={`${theme ? 'dark-theme' : 'bg-[#f0f0f0]'} relative flex flex-col items-center rounded-[20px] w-[700px] max-w-[95%] mx-auto  bg-clip-border shadow-3xl shadow-shadow-500  p-3`}>
                         <div className="mt-2 mb-8 w-full ">
                             <h4 className="px-2 text-xl font-bold text-navy-700 ">
                                 {assignmentDataId?.title}
@@ -30,25 +30,25 @@ const Details = () => {
                             </p>
                         </div>
                         <div className="grid grid-cols-2 gap-4 px-2 w-full ">
-                            <div className={`${theme ? 'bg-purple-950' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
+                            <div className={`${theme ? 'bg-gray-600' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
                                 <p className="text-sm ">Defficulty</p>
                                 <p className="text-base font-medium text-navy-700 ">
                                     {assignmentDataId?.difficulty}
                                 </p>
                             </div>
-                            <div className={`${theme ? 'bg-purple-950' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
+                            <div className={`${theme ? 'bg-gray-600' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
                                 <p className="text-sm ">Date</p>
                                 <p className="text-base font-medium text-navy-700 ">
                                     {assignmentDataId?.date}
                                 </p>
                             </div>
-                            <div className={`${theme ? 'bg-purple-950' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
+                            <div className={`${theme ? 'bg-gray-600' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
                                 <p className="text-sm ">Marks</p>
                                 <p className="text-base font-medium text-navy-700 ">
                                     {assignmentDataId?.marks}
                                 </p>
                             </div>
-                            <div className={`${theme ? 'bg-purple-950' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
+                            <div className={`${theme ? 'bg-gray-600' : 'bg-white'} flex flex-col items-start justify-center rounded-2xl  bg-clip-border px-3 py-4  shadow-md`}>
                                 <p className="text-sm ">Creator</p>
                                 <p className="text-base font-medium text-navy-700 ">
                                     {assignmentDataId?.user}
